569ef6feed9b8408c7a2fad6921625962a0a3ed1
KatieOS <katieosullivandev@gmail.com>
2016-12-01 15:14:03 +0000

    first commit

---
+++ event/First_Event.js
@@ -0,0 +1,9 @@
+// ====================================================================================================
+//
+// Cloud Code for First_Event, write your code here to customize the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
+//
+// ====================================================================================================
+var player = Spark.getPlayer();
+player.credit1(20, "cause I want to");
